0d6c8d8d650976fbe8105c2a8cccc74005694ef3
sbx_user1051 <sbx_user1051@169.254.50.93>
2019-04-08 15:54:02 +0000

    Mon Apr 08 15:54:02 UTC 2019

--- test/FIZ/fizz_buzz_test.rb
+++ test/FIZ/fizz_buzz_test.rb
@@ -82,16 +82,21 @@ class ClientTest < Minitest::Test
     assert_equal "deluxe", FizzBuzz.new.fizz_buzz(22)
   end
 
-  def test_fizz_deluxe_is_returned_when_given_33
+  def test_fizz_fake_deluxe_is_returned_when_given_33
     assert_equal "fizz fake deluxe", FizzBuzz.new.fizz_buzz(33)
   end
 
-  def test_buzz_deluxe_is_returned_when_given_55
+  def test_buzz_fake_deluxe_is_returned_when_given_55
     assert_equal "buzz fake deluxe", FizzBuzz.new.fizz_buzz(55)
   end
 
-  def test_fizz_buzz_deluxe_is_returned_when_given_555
+  def test_fizz_buzz_fake_deluxe_is_returned_when_given_555
     assert_equal "fizz buzz fake deluxe", FizzBuzz.new.fizz_buzz(555)
   end
+
+  def test_fizz_fake_deluxe_is_returned_when_given_555
+    assert_equal "fizz fake deluxe", FizzBuzz.new.fizz_buzz(333)
+  end
 end
+
 
