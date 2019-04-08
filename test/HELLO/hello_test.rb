15d2d161dd585254731864f892e1b0f633eebc15
sbx_user1051 <sbx_user1051@169.254.156.1>
2019-04-08 11:19:19 +0000

    Mon Apr 08 11:19:19 UTC 2019

--- test/HELLO/hello_test.rb
+++ test/HELLO/hello_test.rb
@@ -3,11 +3,13 @@ require 'logging'
 
 Logging.logger.root.appenders = Logging.appenders.stdout
 
-require_solution 'SUM'
+require_solution 'HLO'
 
 class ClientTest < Minitest::Test
+
   def return_message
-    assert_equal 'An '
+    assert_equal 'Hello Ignore', Hello.new.hello('Ignore')
   end
 
 end
+
