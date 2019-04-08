a1eea015f00d2b1cd81c5e43e69a9b605375c89f
sbx_user1051 <sbx_user1051@169.254.156.1>
2019-04-08 11:22:19 +0000

    Mon Apr 08 11:22:19 UTC 2019

--- test/HELLO/hello_test.rb
+++ test/HELLO/hello_test.rb
@@ -8,8 +8,9 @@ require_solution 'HLO'
 class ClientTest < Minitest::Test
 
   def return_message
-    assert_equal 'Hello Ignore', Hello.new.hello('Ignore')
+    assert_equal 'Hello Ignore!!!!!!', Hello.new.hello('Ignore')
   end
 
 end
+
 
